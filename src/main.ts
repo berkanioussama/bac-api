4bfbf40631c250260c032197ceb87ef444b6b757
Berkani oussama ala eddine <berkanioussama@gmail.com>
2025-02-18 19:32:08 +0100

    Update main.ts add frontend url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://bac-kanban.vercel.app', // Replace with your frontend URL
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
