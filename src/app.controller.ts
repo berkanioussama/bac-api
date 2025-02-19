e3ce72b8b06cd2b4178e584d1ec574869ac70093
Berkani oussama ala eddine <berkanioussama@gmail.com>
2025-02-19 09:36:50 +0100

    Update app.controller.ts fix get lessens and get lessons by trimestre

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Class } from './data/classes';
-import { Lesson } from './data/lessons';
+import { LessonsService } from './lessons.service';
+import { Lesson } from '../data/lessons';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+@Controller('lessons')
+export class LessonsController {
+  
+  constructor(private readonly lessonsService: LessonsService) {}
+  
   @Get()
-  getClasses(): Class[] {
-    return this.appService.getClasses();
+  getLessons(): Lesson[] {
+    return this.lessonsService.getLessons();
   }
 
-  @Get('islamic-science')
-  getLessensIslamicScince(): Lesson[] {
-    return this.appService.getLessensIslamicScince();
+  @Get('trimestre/:trimestre')
+  getLessonsByTrimestre(@Param('trimestre') trimestre: string): Lesson[] {
+    const trimestreNumber = parseInt(trimestre, 10);
+    return this.lessonsService.getLessonsByTrimestre(trimestreNumber);
   }
-
-  @Get('islamic-science/trimestre/:trimestre')
-  getLessensIslamicScinceByTrimestre(@Param('trimestre') trimestre: number): Lesson[] {
-    const trimestreNumber = +trimestre;
-    return this.appService.getLessensIslamicScinceByTrimestre(trimestreNumber);
-  }
-
-  @Get('islamic-science/lesson/:lesson')
-  getLessensIslamicScinceByLessonId(@Param('lesson') lesson: string): Lesson | undefined {
-    const lessonId = String(+lesson) ;
-    return this.appService.getLessensIslamicScinceByLessonId(lessonId);
-  }
+  
 }
