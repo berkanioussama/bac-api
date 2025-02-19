7c8dd5d4da88c840cb0af7d1640e77865ecbf2eb
Berkani oussama ala eddine <berkanioussama@gmail.com>
2025-02-19 09:37:47 +0100

    Update app.service.ts fix get lessons and by trimestre

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { Class, classes } from './data/classes';
-import { Lesson, lessons } from './data/lessons';
+import { Lesson, lessons } from 'src/data/lessons';
 
 @Injectable()
-export class AppService {
-  getClasses(): Class[] {
-    return classes;
+  
+export class LessonsService {
+  getLessons(): Lesson[] {
+    return lessons;
   }
 
-  getLessensIslamicScince(): Lesson[] {
-    return lessons.filter((lesson) => lesson.classId === '1');
-  }
-
-  getLessensIslamicScinceByTrimestre(trimestreNumber: number): Lesson[] {
-    return lessons.filter((lesson) => lesson.classId === '1').filter((lesson) => lesson.trimestre === trimestreNumber);
-  }
-
-  getLessensIslamicScinceByLessonId(lessonId: string): Lesson | undefined {
-    return lessons.filter((lesson) => lesson.classId === '1').find((lesson) => lesson.id === lessonId);
+  getLessonsByTrimestre(trimestre: number): Lesson[] {
+    return lessons.filter((lesson) => lesson.trimestre === trimestre);
   }
 }
